3e3db05c46dd76ffc2f0c6f5100e7023ec22ca58
drop-dan <daniel@earnwithdrop.com>
2017-12-06 00:54:59 -0500

    Changes background color

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -3,7 +3,7 @@ import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#394C88'
+    this.stage.backgroundColor = '#1E2F67'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
