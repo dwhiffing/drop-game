46bee3a3b92247ded2f0fdba31bea5ec82035117
drop-dan <daniel@earnwithdrop.com>
2017-12-13 13:38:12 -0500

    Updates dist

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -18,8 +18,8 @@ export default class extends Phaser.State {
       },
       active: this.fontsLoaded
     })
-    // this.load.image('loaderBg', './assets/images/loader-bg.png')
-    // this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('loaderBg', './assets/images/loader-bg.png')
+    this.load.image('loaderBar', './assets/images/loader-bar.png')
   }
 
   render () {
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -4,6 +4,22 @@ export default class extends Phaser.State {
   init () {}
 
   preload () {
+    this.loaderBg = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBg'
+    )
+    this.loaderBar = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBar'
+    )
+    this.loaderBg.anchor.setTo(0.5)
+    this.loaderBg.scale.setTo(window.scaleRatio)
+    this.loaderBar.anchor.setTo(0.5)
+    this.loaderBar.scale.setTo(window.scaleRatio)
+    this.load.setPreloadSprite(this.loaderBar)
+
     this.load.image('menu', './assets/images/menu.png')
     this.load.spritesheet('tile', 'assets/images/sprites.png', 600, 600)
     this.load.spritesheet('flakes', 'assets/images/flakes.png', 320, 320)
