10108bc3ce7f5ba70a879d3ed9ccb1a77fb668b3
drop-dan <daniel@earnwithdrop.com>
2018-05-07 15:00:35 -0400

    Adds branch link modification param

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -1,5 +1,15 @@
 import Phaser from 'phaser'
 
+function getParameterByName (name, url) {
+  if (!url) url = window.location.href
+  name = name.replace(/[[\]]/g, '\\$&')
+  let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  let results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
 export default class extends Phaser.State {
   init (args = {}) {
     this.score = args.score
@@ -21,7 +31,8 @@ export default class extends Phaser.State {
     image.anchor.set(0.5)
     image.scale.set(scale > scaleY ? scaleY : scale)
     this.game.input.onDown.add(() => {
-      window.location = 'https://b.ewd.io/agtq/FERNNNZyGI'
+      const branch = getParameterByName('branch') || 'FERNNNZyGI'
+      window.location = `https://b.ewd.io/${branch}`
     }, this)
   }
 }
